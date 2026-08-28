318d0d43bb4814d4e4d5466145a2a35badd28db3
Denis <denis@MacBook-Air-von-Anna.local>
2026-08-28 09:02:03 +0200

    rebuild the Parser made a new class Waypoint and also changed the DroneConfig to RouteConfig changed the parsing logic to a more pydantic way and used more regex groups to split the txt file

--- src/parser.py
+++ src/parser.py
@@ -2,28 +2,63 @@ import re
 import pydantic
 
 
-class DroneConfig(pydantic.BaseModel):
+class RouteConfig(pydantic.BaseModel):
     nb_drones: int = pydantic.Field(
         gt=0, description="Must be a positive integer")
+    waypoints: list[Waypoint] = pydantic.Field(
+        default_factory=list
+    )
+    connections:list[str] = pydantic.Field(
+        default_factory=list
+    )
+
+class Waypoint(pydantic.BaseModel):
+    role: str
+    name: str
+    x: str
+    y: str
+    color: str
 
 
 class Parser():
     def __init__(self, file_path):
         self.file_path = file_path
 
-    class ParseError(Exception):
-        pass
 
-    def parse(self, file_path):
-        with open(file_path, 'r') as file:
-            content = file.read()
-            for index, item in enumerate(content.splitlines(), start=1):
-                if item.strip():
-                    match = re.match(r'^\s*nb_drones\s*:\s*(.+)\s*$', item)
-                    if match:
-                        if match.group(1).strip().isdigit():
-                            self.nb_drones = int(match.group(1).strip())
-                        else:
-                            raise self.ParseError(
-                                f"Invalid value for nb_drones at line "
-                                f"{index}: {match.group(1).strip()}")
+    def parse(self) -> RouteConfig:
+
+        raw_data = {
+            "waypoints":[],
+            "connections": []
+            }
+        with open(self.file_path, 'r') as file:
+            for index, line in enumerate(file, start=1):
+                line = line.strip():
+                if not line:
+                    continue
+                match_drones = re.match(r'^\s*nb_drones\s*:\s*(.+)\s*$', item)
+                if match_drones:
+                    if match_drones:
+                        raw_data["nb_drones"].append(match_drones.group(1).strip())
+                        continue
+                match_hub = re.match(r'^(start_hub|hub|end_hub)\s*:\s*([a-zA-Z0-9_]+)\s+(\d+)\s+(\d+)\s+\[color=([^\]]+)\]', line)
+                if match_hub:
+                    raw_data["waypoints"].append({
+                        "role": match_hub.group(1),
+                        "name": match_hub.group(2),
+                        "x": match_hub.group(3),
+                        "y": match_hub.group(4),
+                        "color": match_hub.group(5)
+                    })
+                    continue 
+                match_conn = re.match(r'^connection\s*:\s*(.+)$',line)
+                if match_conn:
+                    raw_data["connections"].append(match_conn.group(1).strip())
+                    continue
+
+                try:
+                    return(RouteConfig.model_validation(raw_data))
+                except pydantic.ValidationError as e:
+                    print(f"Fehler beim Validieren der Datei {self.file_path}:")
+                    print(e)
+                    raise
